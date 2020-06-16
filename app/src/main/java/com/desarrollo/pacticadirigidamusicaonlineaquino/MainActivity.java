a52671d350541de9220acc1a3af011ac56555bcf
Virginia Aquino <37305214+virginiayjd7@users.noreply.github.com>
2020-06-16 06:37:32 -0500

    k

--- app/src/main/java/com/desarrollo/pacticadirigidamusicaonlineaquino/MainActivity.java
+++ app/src/main/java/com/desarrollo/pacticadirigidamusicaonlineaquino/MainActivity.java
@@ -1,137 +1,150 @@
 package com.desarrollo.pacticadirigidamusicaonlineaquino;
 
-import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.net.Uri;
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.provider.MediaStore;
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
-
-import com.google.android.material.tabs.TabLayout;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-public  static  final  int REQUEST_CODE=1;
-public  static  ArrayList<MusicFiles>musicFiles;
+    Button play_pause;
+   Button  btn_repetir;
+    MediaPlayer mp;
+    ImageView iv;
+    int repetir = 2, posicion = 0;
+
+    MediaPlayer vectormp [] = new MediaPlayer [3];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        permission();
 
+        play_pause = (Button)findViewById(R.id.btn_play);
+        btn_repetir = (Button) findViewById(R.id.btn_repetir);
+        iv = (ImageView)findViewById(R.id.imageView);
 
-    }
-
-    private void permission() {
-        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        != PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest
-            .permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE);
-        }
-        else {
-
-             musicFiles=getALLAudio(this);
-            initViewPager();
-        }
+        vectormp[0] = MediaPlayer.create(this, R.raw.i);
+        vectormp[1] = MediaPlayer.create(this, R.raw.m);
+        vectormp[2] = MediaPlayer.create(this, R.raw.o);
 
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        super.onRequestPermissionsResult(requestCode,permissions,grantResults);
-        if (requestCode==REQUEST_CODE){
-            if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
 
-                musicFiles=getALLAudio(this);
-                initViewPager();
+    public void PlayPause(View view){
+        if(vectormp[posicion].isPlaying()){
+            vectormp[posicion].pause();
+            play_pause.setBackgroundResource(R.drawable.ic_baseline_play_arrow_24);
+            Toast.makeText(this, "Pausa", Toast.LENGTH_SHORT).show();
+        } else {
+            vectormp[posicion].start();
+            play_pause.setBackgroundResource(R.drawable.ic_baseline_pause_circle_filled_24);
+            Toast.makeText(this, "Play", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Método para el botón Stop
+    public void Stop(View view){
+        if(vectormp[posicion] != null){
+            vectormp[posicion].stop();
+
+            vectormp[0] = MediaPlayer.create(this, R.raw.i);
+            vectormp[1] = MediaPlayer.create(this, R.raw.m);
+            vectormp[2] = MediaPlayer.create(this, R.raw.o);
+            posicion = 0;
+            play_pause.setBackgroundResource(R.drawable.ic_baseline_play_arrow_24);
+            iv.setImageResource(R.drawable.uno);
+            Toast.makeText(this, "Stop", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void Repetir(View view){
+        if(repetir == 1){
+            btn_repetir.setBackgroundResource(R.drawable.ic_baseline_chevron_left_24);
+            Toast.makeText(this, "No repetir", Toast.LENGTH_SHORT).show();
+            vectormp[posicion].setLooping(false);
+            repetir = 2;
+        } else {
+            btn_repetir.setBackgroundResource(R.drawable.ic_baseline_repeat_24);
+            Toast.makeText(this, "Repetir", Toast.LENGTH_SHORT).show();
+            vectormp[posicion].setLooping(true);
+            repetir = 1;
+        }
+    }
+
+    public void Siguiente(View view){
+        if(posicion < vectormp.length -1){
+
+            if(vectormp[posicion].isPlaying()){
+                vectormp[posicion].stop();
+                posicion++;
+                vectormp[posicion].start();
+
+                if(posicion == 0){
+                    iv.setImageResource(R.drawable.uno);
+                } else if(posicion == 1){
+                    iv.setImageResource(R.drawable.uno);
+                }else if(posicion == 2){
+                    iv.setImageResource(R.drawable.uno);
+                }
+
+            } else {
+                posicion++;
+
+                if(posicion == 0){
+                    iv.setImageResource(R.drawable.uno);
+                } else if(posicion == 1){
+                    iv.setImageResource(R.drawable.uno);
+                }else if(posicion == 2){
+                    iv.setImageResource(R.drawable.uno);
+                }
             }
-            else {
-                ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest
-                        .permission.WRITE_EXTERNAL_STORAGE},REQUEST_CODE);
+
+        } else {
+            Toast.makeText(this, "No hay más canciones", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //Método para regresar a la canción anterior
+    public void Anterior(View view){
+        if(posicion >= 1){
+
+            if(vectormp[posicion].isPlaying()){
+                vectormp[posicion].stop();
+                vectormp[0] = MediaPlayer.create(this, R.raw.i);
+                vectormp[1] = MediaPlayer.create(this, R.raw.m);
+                vectormp[2] = MediaPlayer.create(this, R.raw.o);
+
+                posicion--;
+
+                if(posicion == 0){
+                    iv.setImageResource(R.drawable.uno);
+                } else if(posicion == 1){
+                    iv.setImageResource(R.drawable.uno);
+                }else if(posicion == 2){
+                    iv.setImageResource(R.drawable.uno);
+                }
+
+                vectormp[posicion].start();
+
+            } else {
+                posicion--;
+
+                if(posicion == 0){
+                    iv.setImageResource(R.drawable.uno);
+                } else if(posicion == 1){
+                    iv.setImageResource(R.drawable.uno);
+                }else if(posicion == 2){
+                    iv.setImageResource(R.drawable.uno);
+                }
             }
+
+        } else {
+            Toast.makeText(this, "No hay más canciones", Toast.LENGTH_SHORT).show();
         }
     }
 
-
-    private void initViewPager() {
-        ViewPager viewPager=findViewById(R.id.viewpager);
-        TabLayout tabLayout =findViewById(R.id.tab_layout);
-        ViewPagerAdapter viewPagerAdapter= new ViewPagerAdapter(getSupportFragmentManager());
-        ViewPagerAdapter.addFragments(new SongsFragment(), "Songs");
-        ViewPagerAdapter.addFragments(new AlbumFragment(), "Album");
-        viewPager.setAdapter(viewPagerAdapter);
-        tabLayout.setupWithViewPager(viewPager);
-
-    }
-
-    public static  class  ViewPagerAdapter extends FragmentPagerAdapter{
-        private static ArrayList<Fragment> fragments;
-        private static ArrayList<String> titles;
-
-        public  ViewPagerAdapter(@NonNull FragmentManager fm){
-            super(fm);
-            this.fragments=new ArrayList<>();
-            this.titles=new ArrayList<>();
-        }
-        static void addFragments(Fragment fragment, String title){
-            fragments.add(fragment);
-            titles.add(title);
-        }
-
-        @NonNull
-        @Override
-        public Fragment getItem(int position){ return  fragments.get(position); }
-        @Override
-        public  int getCount(){ return fragments.size(); }
-        @Nullable
-        @Override
-        public  CharSequence getPageTitle(int position){
-            return titles.get(position);
-
-        }
-    }
-    public  static  ArrayList<MusicFiles> getALLAudio(Context context){
-        ArrayList<MusicFiles> tempAudioList=new ArrayList<>();
-        Uri uri= MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-        String[] projection= {
-                MediaStore.Audio.Media.ALBUM,
-                MediaStore.Audio.Media.TITLE,
-                MediaStore.Audio.Media.DURATION,
-                MediaStore.Audio.Media.DATA,
-                MediaStore.Audio.Media.ARTIST,
-        };
-        Cursor cursor=context.getContentResolver().query(uri,projection, null, null, null);
-        if(cursor !=null){
-            while (cursor.moveToNext()){
-                String album=cursor.getString(0);
-                String title=cursor.getString(1);
-                String duration=cursor.getString(2);
-                String path=cursor.getString(3);
-                String artist=cursor.getString(4);
-
-                MusicFiles musicFiles=new MusicFiles(path,title,artist,album,duration);
-
-                Log.e("Path:"+ path,"Album");
-                tempAudioList.add(musicFiles);
-            }
-            cursor.close();
-        }
-      return tempAudioList;
-
-    }
 }
